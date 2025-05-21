f36bd41f09800f182d0cf9a9b03a6a3ee3c76b47
Aman Singh <102499131+AmanSingh881@users.noreply.github.com>
2025-05-21 13:57:29 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,9 +1,23 @@
-#!/bin/bash
+#! /bin/bash
 
-VAR1="static-value-1"
+AWS_REGION="us-east-1"
+AWS_ACCOUNT_ID="598361629012"
+ECR_REPO_NAME="frontend"
+CONTAINER_NAME="frontend_container"
+HOST_PORT=80
+CONTAINER_PORT=80
 IMAGE_TAG="__VERSION__"
-VAR3="static-value-3"
 
-echo "VAR1: $VAR1"
-echo "VAR2: $IMAGE_TAG"
-echo "VAR3: $VAR3"
+# Log in to ECR
+aws ecr get-login-password --region ${AWS_REGION} \
+  | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+# Pull the image
+docker pull ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_TAG}
+
+# Run the container (detached, restart on failure)
+docker run -d \
+  --name ${CONTAINER_NAME} \
+  -p ${HOST_PORT}:${CONTAINER_PORT} \
+  --restart unless-stopped \
+  ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_NAME}:${IMAGE_TAG}
